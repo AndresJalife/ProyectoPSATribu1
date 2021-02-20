668d7dd951386956ed3dd66c870c85ef7abcb755
Andres Jalife <ajalife@fi.uba.ar>
2021-02-20 16:38:01 -0300

    obtengo proyectos

--- src/pages/Proyecto.js
+++ src/pages/Proyecto.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
+import {Proyecto} from '../'
 
 export default class Proyecto extends Component {
+
+    constructor(){
+        super();
+        this.obtenerProyectos();
+    }
+
+    agregarProyecto(){
+
+    }
+
+    obtenerProyectos(){
+        let listaProyectos= [];
+
+        function reqListener() {
+            let data = JSON.parse(this.responseText);
+            data.map(proyecto => {listaProyectos.append(
+                <Proyecto dataProyecto={proyecto} />
+            )});
+        }
+          
+        function reqError(err) {
+            console.log('Fetch Error :-S', err);
+        }
+          
+        let getReq = new XMLHttpRequest();
+        getReq.onload = reqListener;
+        getReq.onerror = reqError;
+        getReq.open('GET', 'https://proyectopsa.herokuapp.com/proyectos', true);
+        getReq.send();
+
+
+    }
+
     render() {
         return (
             <div class='proyectosContainer'>
                 <p>Proyectos</p>
                 <button class='botonAgregarProyecto' onClick={this.agregarProyecto()}>Agregar Proyecto</button>
-                +
-                <div class='listaProyecto'>
-
-                </div>
+                
+                
             </div>
         )
     }
